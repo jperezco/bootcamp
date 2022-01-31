67639eb47306a0e5239ed89f8595970a372ef49d
jperezco <javier.perez-cordoba@capgemini.com>
2022-01-31 14:03:15 +0100

    Ejercicio cadena

--- Backend/Ejercicio.1/src/capgemini/Cadena.java
+++ Backend/Ejercicio.1/src/capgemini/Cadena.java
@@ -6,18 +6,25 @@ public class Cadena {
 		String cadena = "3+4+3.4-7*1=";
 		//La dividimos en caracteres y la pasamos a una variable char.
 		char cadena_div[] = cadena.toCharArray();
-
+		String num = "";
+		String operaciones = "";
+		
+		System.out.println(cadena);
 
 		//La vamos recorriendo. Si es un numero imprimimos sin pasar de linea.
 		//Si no lo es imprimimos pasando de línea.
 		for(int i=0; i < cadena_div.length; i++) {
 			if (Character.isDigit(cadena_div[i]) || 
 				cadena_div[i] == '.') {
-				System.out.print(cadena_div[i]);
-				//numeros[] = (double) cadena_div[i];
+				num += cadena_div[i];
+				operaciones += " ";
 			}else {
-				System.out.println(" " + cadena_div[i]);
+				num += " ";
+				operaciones += cadena_div[i];
 			}		
 		}
+		
+		System.out.println(num);
+		System.out.println(operaciones);
 	}
 }
