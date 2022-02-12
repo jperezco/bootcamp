df931bb55f6f153f5400fee956a0208b17b0a45d
jperezco <javier.perez-cordoba@capgemini.com>
2022-02-12 12:46:42 +0100

    Ajedrez

---
+++ Backend/Juego Ajedrez/src/Ajedrez/Juego.java
@@ -0,0 +1,78 @@
+package Ajedrez;
+
+public class Juego {
+	
+	private Tablero elTablero;
+	private Color elTurno;
+	private boolean partidaActiva = false;
+	
+	public Object dameTablero() {
+		return elTablero.clone();
+	}
+	
+	public Color dameTurno() {
+		return elTurno;
+	}
+	
+	public void inicializar() throws Exception{
+		
+		Torre torreBlanca = new Torre(Color.BLANCO);
+		Caballo caballoBlanca = new Caballo(Color.BLANCO);
+		Alfil alfilBlanca = new Alfil(Color.BLANCO);
+		Dama damaBlanca = new Dama(Color.BLANCO);
+		Rey reyBlanca = new Rey(Color.BLANCO);
+		Peon peonBlanca = new Peon(Color.BLANCO);
+		
+		elTablero.ponPieza(torreBlanca, 1, 1);
+		elTablero.ponPieza(caballoBlanca, 1, 2);
+		elTablero.ponPieza(alfilBlanca, 1, 3);
+		elTablero.ponPieza(damaBlanca, 1, 4);
+		elTablero.ponPieza(reyBlanca, 1, 5);
+		elTablero.ponPieza(alfilBlanca, 1, 6);
+		elTablero.ponPieza(caballoBlanca, 1, 7);
+		elTablero.ponPieza(torreBlanca, 1, 8);		
+		for (int i = 1; i <= 8; i++) {
+			elTablero.ponPieza(peonBlanca, 2, i);
+		}
+		
+		Torre torreNegra = new Torre(Color.NEGRO);
+		Caballo caballoNegra = new Caballo(Color.NEGRO);
+		Alfil alfilNegra = new Alfil(Color.NEGRO);
+		Dama damaNegra = new Dama(Color.NEGRO);
+		Rey reyNegra = new Rey(Color.NEGRO);
+		Peon peonNegra = new Peon(Color.NEGRO);
+		
+		elTablero.ponPieza(torreNegra, 8, 1);
+		elTablero.ponPieza(caballoNegra, 8, 2);
+		elTablero.ponPieza(alfilNegra, 8, 3);
+		elTablero.ponPieza(damaNegra, 8, 4);
+		elTablero.ponPieza(reyNegra, 8, 5);
+		elTablero.ponPieza(alfilNegra, 8, 6);
+		elTablero.ponPieza(caballoNegra, 8, 7);
+		elTablero.ponPieza(torreNegra, 8, 8);
+		for (int i = 1; i <= 8; i++) {
+			elTablero.ponPieza(peonNegra, 7, i);
+		}
+		
+		partidaActiva = true;
+		elTurno = Color.BLANCO;
+		
+	}
+	
+	public void jugada(String jugada) throws Exception{
+		Movimiento movimiento = new Movimiento(jugada);
+		mover(movimiento);
+		cambiaTurno();
+	}
+	
+	private void mover(Movimiento movimiento) throws Exception{
+		elTablero.mover(movimiento);
+	}
+	
+	private void cambiaTurno() {
+		if (elTurno == Color.BLANCO) {
+			elTurno = Color.NEGRO;
+		} else {elTurno = Color.BLANCO;}
+	}
+
+}
--- Backend/Juego Ajedrez/src/Ajedrez/Tablero.java
+++ Backend/Juego Ajedrez/src/Ajedrez/Tablero.java
@@ -3,32 +3,7 @@ package Ajedrez;
 public class Tablero {
 
 	private Pieza[][] piezas = new Pieza[8][8];
-	
-//	piezas[0][0] = new Torre(blancas);
-//	piezas[0][1] = new Caballo(blancas);
-//	piezas[0][2] = new Alfil(blancas);
-//	piezas[0][3] = new Dama(blancas);
-//	piezas[0][4] = new Rey(blancas);
-//	piezas[0][5] = new Alfil(blancas);
-//	piezas[0][6] = new Caballo(blancas);
-//	piezas[0][7] = new Torre(blancas);
-//	for (int i = 0; i <= 7; i++) {
-//		piezas[1][i] = new Peon(blancas);
-//	}
-//	
-//	piezas[7][0] = new Torre(negras);
-//	piezas[7][1] = new Caballo(negras);
-//	piezas[7][2] = new Alfil(negras);
-//	piezas[7][3] = new Dama(negras);
-//	piezas[7][4] = new Rey(negras);
-//	piezas[7][5] = new Alfil(negras);
-//	piezas[7][6] = new Caballo(negras);
-//	piezas[7][7] = new Torre(negras);
-//	for (int i = 0; i <= 7; i++) {
-//		piezas[6][i] = new Peon(negras);
-//	}
-//	
-	
+		
 	public Pieza damePieza(int fila, int columna) throws Exception{
 		
 		if (esValido(fila)) {}
