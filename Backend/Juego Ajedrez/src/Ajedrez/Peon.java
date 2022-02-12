9b10a652ae1ec1b83782fc19c9a014dc9c94e4f0
jperezco <javier.perez-cordoba@capgemini.com>
2022-02-12 13:25:43 +0100

    Peon

--- Backend/Juego Ajedrez/src/Ajedrez/Peon.java
+++ Backend/Juego Ajedrez/src/Ajedrez/Peon.java
@@ -9,5 +9,18 @@ public class Peon extends Pieza{
 	protected boolean esValido(Movimiento movimiento, Tablero tablero) {
 		return true;
 	}
-
+	
+	private boolean avanza(Movimiento movimiento) {
+		if (dameColor() == Color.BLANCO && movimiento.deltaColumna() == 1 || 
+				dameColor() == Color.NEGRO && movimiento.deltaColumna() == -1) {
+			return true;
+		} else {return false;}
+	}
+	
+	private boolean inicia(Posicion posicion) {
+		if (dameColor() == Color.BLANCO && posicion.fila() == 2 ||
+				dameColor() == Color.NEGRO && posicion.fila() == 7) {
+			return true;
+		} else {return false;}
+	}
 }
