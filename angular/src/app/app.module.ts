4521131624849172b0ae1969ce7c7edb32165fe0
jperezco <javier.perez-cordoba@capgemini.com>
2022-03-22 19:21:40 +0100

    Primeros Módulos y Servicios de Angular

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { MyCoreModule } from 'src/lib/my-core';
+import { environment } from 'src/environments/environment';
+import { ERROR_LEVEL, LoggerService, MyCoreModule } from 'src/lib/my-core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonServicesModule } from './common-services';
 import { MainModule } from './main';
 import { SecurityModule } from './security';
+import { DemosComponent } from './demos/demos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemosComponent
   ],
   imports: [
     BrowserModule, FormsModule,
-    AppRoutingModule, MainModule, SecurityModule, MyCoreModule
+    AppRoutingModule, MainModule, SecurityModule, MyCoreModule, CommonServicesModule,
+    MainModule,
   ],
-  providers: [],
+  providers: [
+    LoggerService,
+    {provide: ERROR_LEVEL, useValue: environment.ERROR_LEVEL},
+  ],
+  // El componente de arranque es el appComponent. Instancia esa clase.
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ angular/src/lib/my-core/services/logger.service.ts
@@ -0,0 +1,46 @@
+import { Inject, Injectable, InjectionToken, Optional } from '@angular/core';
+
+export const ERROR_LEVEL = new InjectionToken<string>('ERROR_LEVEL')
+
+@Injectable()
+export class LoggerService {
+
+  private level: number = 99;
+
+  constructor(@Optional() @Inject(ERROR_LEVEL) error_level?: number) {
+    if(error_level || error_level === 0) {
+      this.level = error_level;
+    }
+
+  }
+
+  public error(message: String): void {
+    if(this.level > 0){
+      console.error(message);
+    }
+
+  }
+
+  public warn(message: String): void {
+    if(this.level > 1){
+      console.warn(message);
+    }
+  }
+
+  public info(message: String): void {
+    if(this.level > 2){
+      if(console.info) {
+        console.info(message);
+      } else {
+        console.log(message);
+      }
+    }
+  }
+
+  public log(message: String): void {
+    if(this.level > 3){
+      console.log(message);
+    }
+
+  }
+}
